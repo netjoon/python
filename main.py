d875a06e29c86b5ea936031fc320ad3d4448004c
sangjoon.jung <sangjoon.jung@lge.com>
2020-12-29 10:55:45 -0800

    add reverse function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("hello")
+
+def reverse(a) -> 123:
+    rev = 0
+    while a:
+        mod = a % 10
+        a //= 10
+        rev = rev * 10 + mod
+
+    return rev
+
+res = reverse(431)
+print(res)
